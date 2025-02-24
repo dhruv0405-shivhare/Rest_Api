05c6aacc3510ff2bcf349a4569f75bcf1edef1c1
Dhruv <dshivhare661@gmail.com>
2025-02-24 21:24:01 +0530

    Details

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 # from .import views
 from app.views import Stu_List
+from app.views import Stu_Details
 urlpatterns = [
     # path('stu_list/',views.stu_list),
     # path('stu_details/<int:pk>',views.stu_details)
-    path('Stu_List/',Stu_List.as_view(),name='Stu_List')
+    path('Stu_List/',Stu_List.as_view(),name='Stu_List'),
+    path('Stu_Details/<int:pk>',Stu_Details.as_view(),name='Stu_Details')
 ]
