02e374987d79796d7612cc68eb0a4bc13e69dfb9
Dhruv <dshivhare661@gmail.com>
2025-02-24 15:29:18 +0530

    class based

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -1,20 +1,25 @@
 from rest_framework import serializers
 from .models import Student
-class Stu_serializers(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    stu_name=serializers.CharField(max_length=50)
-    stu_email=serializers.EmailField()
-    stu_contact=serializers.IntegerField()
-    stu_address=serializers.CharField(max_length=50)
+# class Stu_serializers(serializers.Serializer):
+#     id=serializers.IntegerField(read_only=True)
+#     stu_name=serializers.CharField(max_length=50)
+#     stu_email=serializers.EmailField()
+#     stu_contact=serializers.IntegerField()
+#     stu_address=serializers.CharField(max_length=50)
 
-    def create(self, validated_data):
+#     def create(self, validated_data):
 
-        return Student.objects.create(**validated_data)
+#         return Student.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.contact = validated_data.get('contact', instance.contact)
-        instance.address = validated_data.get('address', instance.address)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.email = validated_data.get('email', instance.email)
+#         instance.contact = validated_data.get('contact', instance.contact)
+#         instance.address = validated_data.get('address', instance.address)
+#         instance.save()
+#         return instance
+
+class Stu_serializers(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id','stu_name','stu_email','stu_contact','stu_address']
--- project/app/urls.py
+++ project/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .import views
+# from .import views
+from app.views import Stu_List
 urlpatterns = [
-    path('stu_list/',views.stu_list),
-    path('stu_details/<int:pk>',views.stu_details)
+    # path('stu_list/',views.stu_list),
+    # path('stu_details/<int:pk>',views.stu_details)
+    path('Stu_List/',Stu_List.as_view(),name='Stu_List')
 ]
