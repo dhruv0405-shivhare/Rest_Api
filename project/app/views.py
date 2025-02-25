121feba8a0549f9c70bfc55d422e6e39f0a6cffc
Dhruv <dshivhare661@gmail.com>
2025-02-25 15:28:44 +0530

    generic

--- project/app/views.py
+++ project/app/views.py
@@ -7,31 +7,11 @@ from rest_framework import status
 from rest_framework import mixins
 from rest_framework import generics
 
-
-class Stu_List(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class Stu_List(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = Stu_serializers
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class Stu_Details(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class Stu_Details(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = Stu_serializers
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
